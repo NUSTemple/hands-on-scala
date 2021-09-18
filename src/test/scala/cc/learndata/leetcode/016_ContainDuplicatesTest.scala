e2fc0baefce454c3b8516664e8225ecb7ca84cf9
Tan Peng <tanpeng8847@gmail.com>
2021-09-18 19:33:52 +0800

    add ContainsDuplicate problem

---
+++ src/test/scala/cc/learndata/leetcode/016_ContainDuplicatesTest.scala
@@ -0,0 +1,33 @@
+package cc.learndata.leetcode
+
+import org.scalatest.FunSuite
+
+class ContainDuplicatesTest extends FunSuite {
+
+  // test 1
+  test("test 1") {
+    val testResult =
+      ContainDuplicates.containDuplicates(Array(-1, 0, 3, 5, 9, 12))
+    val expectResult = false
+    assert(testResult == expectResult)
+
+  }
+
+    // test 2
+  test("test 2") {
+    val testResult =
+      ContainDuplicates.containDuplicates(Array(1,2,3,1))
+    val expectResult = true
+    assert(testResult == expectResult)
+
+  }
+
+      // test 3
+  test("test 3") {
+    val testResult =
+      ContainDuplicates.containDuplicates(Array(1,1,1,3,3,4,3,2,4,2))
+    val expectResult = true
+    assert(testResult == expectResult)
+
+  }
+}
